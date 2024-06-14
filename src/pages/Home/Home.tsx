815e058dc12b63b94bc182bf0f82980a20ed3969
Dario Reis <guttyerresjunior1995@gmail.com>
2024-06-14 20:52:25 -0300

    Feat: Add name status

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           presentation="Olá, meu nome é"
           name="Dario Reis"
           profession="Sou um"
-          jobs={["Desenvolvedor FullStack", "Desenvolvedor Web"]}
+          jobs={["Desenvolvedor FullStack", "Desenvolvedor Front-end", "Desenvolvedor Back-end"]}
           description=" Tenho dedicado os últimos três anos ao estudo e prática em desenvolvimento web. Possuo especialização em tecnologias front-end e back-end, com foco em JavaScript e Node.JS. Estou sempre buscando aprender e implementar as melhores práticas no desenvolvimento web para criar experiências de usuário excepcionais."
           buttonHref="contact"
           buttonText="Entrar em contato"
