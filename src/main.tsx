7c81a8ab819217bcc9e8c59b28af077d6c9a3513
Dario Reis <guttyerresjunior1995@gmail.com>
2024-10-14 22:52:01 -0300

    feat(analytics): add Vercel Analytics to track portfolio visitors

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Analytics />
+  </React.StrictMode>
+);
