ab675d7855574bd810802232264a3fc32a4426ad
Dario Reis <dario.reis@probrain.com.br>
2025-06-21 22:52:33 -0300

    chore: atualiza foto de perfil do projeto e descrição de cargos
    Atualização visual do projeto com nova imagem de perfil. Ajustes nos cargos descritos para refletir melhor as responsabilidades atuais.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import Profile from "../../components/Profile/Profile";
-import photo from "../../assets/img/profile.jpg";
+import photo from "../../assets/img/profile.png";
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
           presentation="Olá, meu nome é"
           name="Dario Reis"
           profession="Sou um"
-          jobs={["Desenvolvedor FullStack", "Desenvolvedor Front-end", "Desenvolvedor Back-end"]}
+          jobs={["Desenvolvedor FullStack", "Desenvolvedor Front-end", "Desenvolvedor Back-end", "Front-end Security"]}
           description="Tenho três anos de experiência em desenvolvimento web, com foco tanto no front-end quanto no back-end. Durante esse tempo, me especializei em tecnologias como JavaScript e Node.js, buscando constantemente aprimorar minhas habilidades e aprender as melhores práticas do mercado. Meu objetivo é criar interfaces intuitivas e experiências de usuário de alta qualidade. Além disso, estou sempre aberto a novas oportunidades de aprendizado e desafios que me permitam crescer profissionalmente."
           buttonHref="contact"
           buttonText="Entrar em contato"
